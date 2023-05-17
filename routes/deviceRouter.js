a2d619cbfa1406e215b540c122d87c9e3fc070aa
Sbitysbomi <109456362+Sbitysbomi@users.noreply.github.com>
2023-05-17 16:46:33 +0300

    Initial commit

---
+++ routes/deviceRouter.js
@@ -0,0 +1,9 @@
+const Router = require('express')
+const deviceController = require('../controller/deviceController')
+const router = new Router()
+
+router.post('/', deviceController.create)
+router.get('/', deviceController.getAll)
+router.get('/:id', deviceController.getOne)
+
+module.exports = router
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const Router = require('express')
+const router = new Router()
+const deviceRouter = require('./deviceRouter')
+const userRouter = require('./userRouter')
+const brandRouter = require('./brandRouter')
+const typeRouter = require('./typeRouter')
+
+
+router.use('/user', userRouter)
+router.use('/brand', brandRouter)
+router.use('/type', typeRouter)
+router.use('/device', deviceRouter)
+
+
+module.exports = router
